3ce9f049518ad5bfcea84375ba033182386e5f9e
tomwolf22 <44346056+tomwolf22@users.noreply.github.com>
2026-08-03 16:56:33 +0100

    Update env.py

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -890,7 +890,7 @@ if LICENSE_PUBLIC_KEY:
 
 WEBUI_NAME = os.getenv('WEBUI_NAME', 'Open WebUI')
 
-WEBUI_FAVICON_URL = os.getenv('WEBUI_FAVICON_URL', 'https://openwebui.com/favicon.png')
+WEBUI_FAVICON_URL = 'https://files.alyxai.net/44baef33-3145-4fd6-a525-3c15e0819d93_favicon.ico'
 WEBUI_BUILD_HASH = os.getenv('WEBUI_BUILD_HASH', 'dev-build')
 TRUSTED_SIGNATURE_KEY = os.getenv('TRUSTED_SIGNATURE_KEY', '')
 
